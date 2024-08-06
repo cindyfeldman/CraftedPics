ff6253ea8a7267f0e3c2a1a6acf60b53d3a78dd8
cindyfeldman <30363621+cindyfeldman@users.noreply.github.com>
2024-08-05 21:43:28 -0700

    Changed create album page UI to take a prompt from user.

--- app/src/main/java/com/example/craftedpics/MainActivity.java
+++ app/src/main/java/com/example/craftedpics/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.craftedpics;
 
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,32 +27,65 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 import com.example.craftedpics.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
+
 public class MainActivity extends AppCompatActivity {
 
+
     private ActivityMainBinding binding;
-    private Button newAlbumButton ;
+    ActivityResultLauncher<Intent> resultLauncher;
+    private Button newAlbumButton;
+    private ImageView imageView ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.AlbumsFragment, R.id.CreateAlbumFragment, R.id.SettingsFragment)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
+        View root = binding.getRoot();
+        imageView = findViewById(R.id.image_view);
+        onImageSelected();
+        // newAlbumButton.setOnClickListener(view -> pickImage());
 
     }
+    private void pickImage(){
+        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+        resultLauncher.launch(intent);
+    }
 
+
+
+    private void onImageSelected() {
+        resultLauncher =  registerForActivityResult (
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        try {
+                            Uri imageUri = result.getData().getData();
+                            imageView.setImageURI(imageUri);
+                        }catch(Exception e){
+                            Toast.makeText(MainActivity.this, "No image selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
+
+
+
